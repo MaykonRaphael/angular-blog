d7d3923404313e4adac39483f282a2a879c1dd3a
Maykon Raphael <mraphaeld.batista@gmail.com>
2023-10-05 14:59:29 -0300

    feat: create page content

---
+++ src/app/data/mookData.ts
@@ -0,0 +1,14 @@
+export const data = [
+  {
+    id: 1,
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvffPjX5mzoG1ZoS0phFkXVDf4DzjZihFtQw&usqp=CAU',
+    title: 'Kick Off NLf',
+    description: 'In September on ESPN channels you will watch the best players in the world!'
+  },
+  {
+    id: 2,
+    image: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/4208d2af-232f-433b-a5c1-8af8bcef8efa/d9p053f-6341f2cb-5c91-487c-aaf4-6bcfe904d097.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzQyMDhkMmFmLTIzMmYtNDMzYi1hNWMxLThhZjhiY2VmOGVmYVwvZDlwMDUzZi02MzQxZjJjYi01YzkxLTQ4N2MtYWFmNC02YmNmZTkwNGQwOTcucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.tFL82YsUcQ0_mxD0m0npDlWpEBX4R5XGHAdhqjRoU4I',
+    title: 'New Deadpool movie',
+    description: 'In September you will watch the best in the world!'
+  }
+]
